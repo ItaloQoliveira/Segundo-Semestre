1757291fefcace252214694f473b2fe90c3830e6
ItaloQoliveira <ita.lo500023@gmail.com>
2019-08-15 12:39:53 -0300

    !5/08 12:40

--- Tecnicas De Programação/Exercicio2.c
+++ Tecnicas De Programação/Exercicio2.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <math.h>
 
 void media1 (int n);
 void media2 (int n);
@@ -20,7 +21,7 @@ int main(){
     scanf("%d",&op);
     switch (op){
     case 1:
-        media1(n);
+        media1(n); 
         break;
     case 2:
         media2(n);
@@ -37,27 +38,49 @@ int main(){
 }
 
 void media1(int n){
-    int i,numeros[50],somatotal=0;
-    float resultado;
+    int i;                             
+    float resultado,numeros[50],somatotal=0; //não sei exatamente porque deixei em 50 o maximo, podia ter usado uma variavel. Optei pelo vetor por conta de poder mostrar os velores posteriormente ou coisa do tipo
     for (i=0;i<n;i++){
         printf("\nInsira o número:");
-        scanf("%d",&numeros[i]);
+        scanf("%f",&numeros[i]);
         somatotal+=numeros[i];
     }
     resultado=somatotal/n;
-    printf("A média de todos os %d numeros é de %f ",n,resultado);
+    printf("A média de todos os %d numeros é de %.2f ",n,resultado);
 }
 
 void media2 (int n){
+int i;
+    float resultado,numeros[50],somatotal=0,pesos[50],pesototal=0;                       
+    for (i=0;i<n;i++){
+        printf("\nInsira o número:");
+        scanf("%f",&numeros[i]);
+        printf("\nInsira o peso:");
+        scanf("%f",&pesos[i]);     
+        pesototal+=pesos[i];
+        somatotal+=(numeros[i]*pesos[i]);
+    }
+    resultado=somatotal/pesototal;
+    printf("A média de todos os %d numeros é de %.2f",n,resultado);
 
 }
 
-void media3 (int n){
+void media3 (int n){ 
+    int i; // essa é meio diferente: raiz de indice n do produto de tudo pow(pt,1/n)
+    float produtototal=1,numeros[50],resultado;
+    for (i=0;i<n;i++){
+        printf("\nInsira o numero:");
+        scanf("%f",&numeros[i]);
+        produtototal=(produtototal*numeros[i]);
 
+    }
+                        //importante:Caso não seja 1.0, dá erro.
+    resultado= pow(produtototal,1.0/n); //nota mental: Quando usar a math.h usar o gcc com -lm
+    printf("A media de todos os %d números é de %.2f",n,resultado); 
 
 }
 
 void media4 (int n){
 
-    
+
 }

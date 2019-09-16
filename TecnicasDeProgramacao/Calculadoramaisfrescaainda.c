1724be180f3168d4affbc1aa5eeb8252d2851e60
ItaloQoliveira <ita.lo500023@gmail.com>
2019-09-16 17:05:02 -0300

    16/09 17:04

--- TecnicasDeProgramacao/Calculadoramaisfrescaainda.c
+++ TecnicasDeProgramacao/Calculadoramaisfrescaainda.c
@@ -7,33 +7,36 @@ float divisao(float,float);
 float multi(float,float);
 float subt(float,float);
 int main(){
-    char c[25],operador;
+    char varArquivo,operador;
     float v1,v2;
     FILE *arg;
     arg=fopen("arqcalc.txt","r");
-    do{
-        fgets(c,25,arg);    
-        fscanf(arg,"%f %c %f",&v1,&operador,&v2);
-        if(c!=EOF){
-            switch (operador){
+    varArquivo= fscanf(arg,"%f %c %f",&v1,&operador,&v2);
+    while(varArquivo!=EOF){    
+        
+        switch (operador){
             case '+':
-                printf("%.2f",soma((v1),(v2)));
+                printf("%.2f\n",soma((v1),(v2)));
                 break;
             case '-':
-                printf("%.2f",subt((v1),(v2)));
+                printf("%.2f\n",subt((v1),(v2)));
                 break;
-            case '*':
-                printf("%.2f",multi((v1),(v2)));
+                case '*':
+                printf("%.2f\n",multi((v1),(v2)));
                 break;
             case '/':
-                printf("%.2f",divisao((v1),(v2)));
+                printf("%.2f\n",divisao((v1),(v2)));
                 break;
             default:
                 printf("\nOpção invalida\n");
                 break;
-            }
         }
-    }while(c!=EOF);
+        varArquivo= fscanf(arg,"%f %c %f",&v1,&operador,&v2);
+    }
+
+
+
+    
 }
 float soma(float a,float b){
     return a+b;
